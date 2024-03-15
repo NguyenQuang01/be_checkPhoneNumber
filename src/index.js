4e829bde857dcb7f13b08a82ddab79e01f2d6074
Amj <macos@Mac-mini-cua-MacOS.local>
2024-03-15 08:33:37 +0700

    fix login

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = 3001;
 const routerAccount = require("./router/apiLogin.js");
 const routerEmail = require("./router/apiMail.js");
 const router2 = require("./router/apiLogin.js");
@@ -12,5 +12,5 @@ app.use(bodyParser.json());
 app.use("/v1/", [routerAccount, routerEmail]);
 app.use("/v2/", router2);
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
